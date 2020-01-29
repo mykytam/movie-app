c4e5f19b32b643881a8690b178166f9505a3fa5e
mykytam <morar.nikita@gmail.com>
2020-01-29 12:41:07 +0200

    Added slider indicator with timer

--- app/src/main/java/com/example/movielover/HomeActivity.java
+++ app/src/main/java/com/example/movielover/HomeActivity.java
@@ -5,13 +5,18 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HomeActivity extends AppCompatActivity {
 
     private List<Slide> lstSlides;
     private ViewPager sliderpager;
+    private TabLayout indicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         sliderpager = findViewById(R.id.slider_pager);
+        indicator = findViewById(R.id.indicator);
 
         // prepare a list of slides
         lstSlides = new ArrayList<>();
@@ -30,6 +36,32 @@ public class HomeActivity extends AppCompatActivity {
         SliderPagerAdapter adapter = new SliderPagerAdapter(this, lstSlides);
 
         sliderpager.setAdapter(adapter);
+
+        //setup timer
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new HomeActivity.SliderTimer(), 4000, 6000);
+
+
+        indicator.setupWithViewPager(sliderpager, true);
+    }
+
+    class SliderTimer extends TimerTask {
+
+        @Override
+        public void run() {
+
+            HomeActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (sliderpager.getCurrentItem()<lstSlides.size()-1) {
+                        sliderpager.setCurrentItem(sliderpager.getCurrentItem()+1);
+                    } else {
+                        sliderpager.setCurrentItem(0);
+                    }
+                }
+            });
+
+        }
     }
 
 }
