2d17ed4d6f2303225f69aff72f88f14011cfa7da
mykytam <morar.nikita@gmail.com>
2020-01-31 16:30:54 +0200

    Fixed bugs, renewed gesign

--- app/src/main/java/com/example/movielover/ui/MovieDetailActivity.java
+++ app/src/main/java/com/example/movielover/ui/MovieDetailActivity.java
@@ -3,16 +3,19 @@ package com.example.movielover.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.example.movielover.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MovieDetailActivity extends AppCompatActivity {
 
     private ImageView MovieThumbnailImg, MovieCoverImg;
     private TextView movie_title, movie_desc;
+    private FloatingActionButton play_fab;
 
 
     @Override
@@ -25,18 +28,22 @@ public class MovieDetailActivity extends AppCompatActivity {
 
     void iniViews() {
         // get the data
+        play_fab = findViewById(R.id.play_fab);
         String movieTitle = getIntent().getExtras().getString("title");
         int imageResourceId = getIntent().getExtras().getInt("imgURL");
         int imagecover = getIntent().getExtras().getInt("imgCover");
-        MovieThumbnailImg = findViewById(R.id.detail_movie_cover);
+        MovieThumbnailImg = findViewById(R.id.detail_movie_img);
         Glide.with(this).load(imageResourceId).into(MovieThumbnailImg);
         MovieThumbnailImg.setImageResource(imageResourceId);
-        MovieCoverImg = findViewById(R.id.detail_movie_img);
+        MovieCoverImg = findViewById(R.id.detail_movie_cover);
         Glide.with(this).load(imagecover).into(MovieCoverImg);
         movie_title = findViewById(R.id.detail_movie_title);
         movie_title.setText(movieTitle);
         getSupportActionBar().setTitle(movieTitle);
         movie_desc = findViewById(R.id.detail_movie_desc);
+        //animation
+        MovieCoverImg.setAnimation(AnimationUtils.loadAnimation(this, R.anim.scale_animation));
+        play_fab.setAnimation(AnimationUtils.loadAnimation(this, R.anim.scale_animation));
 
 
     }
